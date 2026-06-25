0f4d50d7e48ff5ac922272f59d2153d80b01c3f9
Tyrell Curry <tyrellcurryio@gmail.com>
2026-06-24 21:51:16 -0700

    feat: sqlite dep added

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.Method, r.URL.Path, r.Header)
+		log.Println(r.Method, r.URL.Path)
 		next.ServeHTTP(w, r)
 	})
 }
@@ -16,5 +17,10 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", loggingMiddleware(fileServer))
 	log.Println("Starting server on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	srv := &http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
 }

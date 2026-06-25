fad0339891af46b060abcd0857728f204bfc5ff8
Tyrell Curry <tyrellcurryio@gmail.com>
2026-06-24 21:35:48 -0700

    feat: basic middleware logging

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -5,6 +5,16 @@ import (
 	"net/http"
 )
 
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.URL.Path, r.Header)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("./static"))))
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", loggingMiddleware(fileServer))
+	log.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
